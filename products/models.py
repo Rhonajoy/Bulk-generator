c672bacf85eb3162f32b3f0e61c54f7a94f64b5f
RhonaJoy <rhonajoy8@gmail.com>
2024-03-18 13:11:31 +0300

    change logic

--- products/models.py
+++ products/models.py
@@ -1,16 +1,29 @@
+# from django.db import models
+
+# # Create your models here.
+# class Product(models.Model):
+#     name=models.CharField(max_length=50)
+#     image=models.CharField(max_length=100)
+# class ProductVariant(models.Model):
+#     sku=models.CharField(max_length=50)
+#     name=models.CharField(max_length=50)
+#     details=models.CharField(max_length=50)
+#     price=models.DecimalField(max_digits=10,decimal_places=2)
+#     product=models.ForeignKey(Product, related_name='variants',on_delete=models.CASCADE,null=False)
+
+
+# # Example: Print the author's name
+    
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
-    name=models.CharField(max_length=50)
-    image=models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    image = models.CharField(max_length=100)
+
 class ProductVariant(models.Model):
-    sku=models.CharField(max_length=50)
-    name=models.CharField(max_length=50)
-    details=models.CharField(max_length=50)
-    price=models.DecimalField(max_digits=10,decimal_places=2)
-    product_id=models.ForeignKey(Product, related_name='variants',on_delete=models.CASCADE)
+    sku = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    details = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    product = models.ForeignKey(Product, related_name='variants', on_delete=models.CASCADE, null = True)
 
-
-# Example: Print the author's name
-    
--- products/serializers.py
+++ products/serializers.py
@@ -1,42 +1,64 @@
+# from rest_framework import serializers
+# from .models import Product,ProductVariant
+
+# class ProductVariantSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model=ProductVariant
+#         fields=['sku','name','details','price' ]
+
+# class ProductSerializer(serializers.ModelSerializer):
+#     variants=ProductVariantSerializer(many=True,read_only=True)
+#     class Meta:
+#         model=Product
+#         fields=['name','image','variants']
+
+#         def create(self, validated_data):
+#             variants_data = validated_data.pop('variants')
+#             product = Product.objects.create(**validated_data)
+#             for variant_data in variants_data:
+#                 ProductVariant.objects.create(product=product, **variant_data)
+#             return product
+            
+            
+#             # variant_data = validated_data.pop('variants')
+#             # product = Product.objects.create(**validated_data)
+
+#             # for variants in variant_data:
+#             #     ProductVariant.objects.create(product=product, **variants)
+
+#             # return product
+            
+            
+#             # product = Product.objects.create(**validated_data)
+#             # ProductVariant.objects.create(product=product **variant_data)
+#             # return product
+            
+
+#             # variants_data = validated_data.pop('variants', [])
+#             # product = Product.objects.create(**validated_data)
+#             # for variant_data in variants_data:
+#             #     ProductVariant.objects.create(product=product, **variant_data)
+#             # return product    
 from rest_framework import serializers
-from .models import Product,ProductVariant
+from .models import Product, ProductVariant
 
 class ProductVariantSerializer(serializers.ModelSerializer):
     class Meta:
-        model=ProductVariant
-        fields=['id','sku','name','details','price', ]
+        model = ProductVariant
+        fields = ['sku', 'name', 'details', 'price']
 
 class ProductSerializer(serializers.ModelSerializer):
-    variants=ProductVariantSerializer(many=True,read_only=True)
+    variants = ProductVariantSerializer(many=True)
+
     class Meta:
-        model=Product
-        fields=['id','name','image','variants']
+        model = Product
+        fields = ['name', 'image', 'variants']
 
-        def create(self, validated_data):
-            variants_data = validated_data.pop('variants', [])
-            product = Product.objects.create(**validated_data)
-            for variant_data in variants_data:
-                ProductVariant.objects.create(product=product, **variant_data)
-                print(variants_data)
-            return product
-            
-            
-            # variant_data = validated_data.pop('variants')
-            # product = Product.objects.create(**validated_data)
+    def create(self, validated_data):
+        variants_data = validated_data.pop('variants')
+        product = Product.objects.create(**validated_data)
+        for variant_data in variants_data:
+            ProductVariant.objects.create(product=product, **variant_data)
+        return product
 
-            # for variants in variant_data:
-            #     ProductVariant.objects.create(product=product, **variants)
-
-            # return product
-            
-            
-            # product = Product.objects.create(**validated_data)
-            # ProductVariant.objects.create(product=product **variant_data)
-            # return product
-            
-
-            # variants_data = validated_data.pop('variants', [])
-            # product = Product.objects.create(**validated_data)
-            # for variant_data in variants_data:
-            #     ProductVariant.objects.create(product=product, **variant_data)
-            # return product    
+    
